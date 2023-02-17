c639d41f372135d30eb16833f60cb415e71e4e8a
VladimirDedov <vldedov@bk.ru>
2023-02-17 17:49:52 +0600

    added captcha

--- dedov_resume/resume/forms.py
+++ dedov_resume/resume/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-
+from captcha.fields import CaptchaField
 
 class SendMailForm(forms.Form):
     name = forms.CharField(max_length=255, label='Ваше имя')
     email = forms.EmailField(max_length=255, label='Ваш E-mail')
     content = forms.CharField(label='Ваше сообщение', widget=forms.Textarea(attrs={'cols':60, 'rows':10}))
+    captcha = CaptchaField(label='Введите код ')
--- dedov_resume/resume/views.py
+++ dedov_resume/resume/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, FormView
 from .models import User
 from .forms import SendMailForm
 
@@ -9,7 +9,7 @@ class ViewIndex(ListView):
     queryset = User.objects.all()
     template_name = 'resume/index.html'
     context_object_name = 'user'
-    print(f'{context_object_name } + 123')
+
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
@@ -17,6 +17,9 @@ class ViewIndex(ListView):
         context['form']=form
         return context
 
+    # def form_valid(self, form):
+    #     print(form.cleaned_data)
+    #     return redirect('index')
 
 # def ViewIndex(request):
 #     return render(request, 'resume/base.html')
